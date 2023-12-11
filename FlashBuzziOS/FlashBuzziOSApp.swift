29fc4bb30c8205fcd910e5cc658f0abeb6deb5b1
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2023-12-11 15:46:32 +0700

    Initial Commit

---
+++ FlashBuzziOS/FlashBuzziOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashBuzziOSApp.swift
+//  FlashBuzziOS
+//
+//  Created by Ridha Ahmad Firdaus on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashBuzziOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
